83ef1053cae9a52295e30e13bdac66aa13bb3971
connorwaldman <waldmanconnor@gmail.com>
2017-04-21 21:36:27 -0400

    Update

--- test.js
+++ test.js
@@ -8,13 +8,15 @@ QUnit.test( "authorize", function( assert ) {
   assert.equal(index.authorize == true, "Passed");
 });
 
-QUnit.test( "start", function( assert ) {
-  var id = realtimeUtils.getParam('id');
-  assert.equal(realtimeUtils.load == id, "Passed");
+QUnit.test( "authorize", function( assert ) {
+  assert.equal(index.authorize == true, "Passed");
 });
 
-QUnit.test( "onFileInitialize", function( assert ) {
-  var string = model.createString();
-  assert.equal(model.getRoot().set == string, "Passed");
+QUnit.test( "authorize", function( assert ) {
+  assert.equal(index.authorize == true, "Passed");
+});
+
+QUnit.test( "authorize", function( assert ) {
+  assert.equal(index.authorize == true, "Passed");
 });
 
