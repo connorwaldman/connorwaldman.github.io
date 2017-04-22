2a026025ccdaf371578512e7bc336280bae8ac8c
connorwaldman <waldmanconnor@gmail.com>
2017-04-21 21:07:07 -0400

    Update test.js

--- test.js
+++ test.js
@@ -8,3 +8,13 @@ QUnit.test( "authorize", function( assert ) {
   assert.equal(index.authorize == true, "Passed");
 });
 
+QUnit.test( "start", function( assert ) {
+  var id = realtimeUtils.getParam('id');
+  assert.equal(realtimeUtils.load == id, "Passed");
+});
+
+QUnit.test( "onFileInitialize", function( assert ) {
+  var string = model.createString();
+  assert.equal(model.getRoot().set == string, "Passed");
+});
+
