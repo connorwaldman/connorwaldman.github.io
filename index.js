ca0652ce039d963cff718963530169f2e67ab764
connorwaldman <waldmanconnor@gmail.com>
2017-06-25 15:38:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ $(document).mousemove(function(a) {
         var t = d / 100 * l / 3e3;
         $(".front").css("opacity", t)
     })
+
+//Changes background color based on mouse position
+$(document).mousemove(function(e){
+    var $width = ($(document).width())/(252 - 23);
+    var $height = ($(document).height())/(253 - 2);
+    var $pageX = parseInt(e.pageX / $width,10) + 23;
+    var $pageY = parseInt(e.pageY / $height,10) + 2;
+    $("body").css("background-color", "rgb("+$pageX+","+$pageY+","+$pageX+")");
+    console.log($pageX, $pageY);
+}); 
